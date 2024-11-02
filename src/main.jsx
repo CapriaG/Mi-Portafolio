f312a1f1cf1ec1795f0f352d01f86dd75ee6cf2b
Gabriel Capria <105518132+CapriaG@users.noreply.github.com>
2024-11-02 00:01:24 -0300

    change main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import './index.css'
+import './index.css';
 import App from './App.jsx'
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
