a01e73fe99d7556ec43469202fb6d643b38ce15f
Gabriel Capria <105518132+CapriaG@users.noreply.github.com>
2024-11-01 23:58:24 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import './index.css'
 import App from './App.jsx'
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
